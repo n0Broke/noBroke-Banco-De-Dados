9ce2fc0905a14b3c41a02dbe6a17c34ee4e9205f
04Richard07 <richard.araujo@sptech.school>
2026-05-21 10:42:47 -0300

    Final

--- Scripts/script-noBroke.sql
+++ Scripts/script-noBroke.sql
@@ -1,4 +1,5 @@
-Create DATABASE noBroke;
+# DROP DATABASE noBroke;
+CREATE DATABASE noBroke;
 USE noBroke;
 
 CREATE TABLE empresa (
@@ -34,10 +35,8 @@ CREATE TABLE usuario (
 CREATE TABLE componente (
     id_componente INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60) NOT NULL,
-    especificacao VARCHAR(60) NOT NULL,
-    capacidade VARCHAR(45) NOT NULL # coluna removida
+    especificacao VARCHAR(60) NOT NULL
 );
-alter table componente drop column capacidade;
 
 CREATE TABLE formato (
     id_formato INT PRIMARY KEY AUTO_INCREMENT,
@@ -63,40 +62,25 @@ CREATE TABLE grupo_pessoas(
   fk_usuario INT NOT NULL,
   fk_servidor INT NOT NULL,
   nome_grupo CHAR(45) NOT NULL,
-  
   PRIMARY KEY (id_grupo, fk_servidor),
-  
-  CONSTRAINT fk_grupo_pessoas_usuario
-  FOREIGN KEY (fk_usuario)
-    REFERENCES usuario (id_usuario), 
-  
-  CONSTRAINT fk_grupo_pessoas_servidor
-    FOREIGN KEY (fk_servidor)
-    REFERENCES servidor (id_servidor));
-
-desc servidor;
+  CONSTRAINT fk_grupo_pessoas_usuario FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario), 
+  CONSTRAINT fk_grupo_pessoas_servidor FOREIGN KEY (fk_servidor) REFERENCES servidor (id_servidor)
+);
 
 CREATE TABLE tipo_componente (
     id_tipo INT AUTO_INCREMENT,
-    fk_componente INT NOT NULL,
     fk_servidor INT NOT NULL,
     fk_formato INT NOT NULL,
+    fk_componente INT NOT NULL,
     nome_componente VARCHAR(45) NOT NULL,
     valor_max_critico DECIMAL(10,2) NOT NULL,
     valor_min_critico DECIMAL(10,2) NOT NULL,
-    valor_max_atencao DECIMAL(10,2) NOT NULL, #coluna removida
-    valor_min_atencao DECIMAL(10,2) NOT NULL, #coluna removida
-    
     PRIMARY KEY (id_tipo, fk_componente, fk_servidor, fk_formato),
-    
     FOREIGN KEY (fk_componente) REFERENCES componente(id_componente),
     FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor),
-    FOREIGN KEY (fk_formato) REFERENCES formato(id_formato)
+    FOREIGN KEY (fk_formato) REFERENCES formato(id_formato),
+    capacidade VARCHAR(45)
 );
-alter table tipo_componente add column capacidade VARCHAR(45); #deveria ser not null, mas vou deixar nulo pra facilitar
-alter table tipo_componente drop column valor_max_atencao;
-alter table tipo_componente drop column valor_min_atencao;
-
 
 CREATE TABLE alerta (
     id_alerta INT PRIMARY KEY AUTO_INCREMENT,
@@ -105,9 +89,7 @@ CREATE TABLE alerta (
     descricao VARCHAR(225) NOT NULL,
     valor_capturado DECIMAL(5,2),
     data_alerta DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    
-    FOREIGN KEY (fk_tipo, fk_componente) 
-        REFERENCES tipo_componente(id_tipo, fk_componente)
+    FOREIGN KEY (fk_tipo, fk_componente) REFERENCES tipo_componente(id_tipo, fk_componente)
 );
 
 
@@ -125,26 +107,11 @@ INSERT INTO funcao (nome_funcao) VALUES
 INSERT INTO formato (unidade_medida) VALUES 
 ('%'), ('GHz'), ('GB'), ('MB/s');
 
-/*
-INSERT INTO componente (nome, especificacao, capacidade) VALUES 
-('Processador', 'Intel Xeon Gold', '3.4'),
-('Memória RAM', 'DDR4', '32'),
-('Disco Rígido', 'SSD NVMe', '1024'),
-('Rede', 'Interface Ethernet', '10');
-*/
-
 INSERT INTO componente (nome, especificacao) VALUES 
 ('Processador', 'Intel Xeon Gold'),
 ('Memória RAM', 'DDR4'),
 ('Disco Rígido', 'SSD NVMe'),
 ('Rede', 'Interface Ethernet');
-
-
-INSERT INTO permissao (descricao, fk_funcao) VALUES 
-('Acesso total ao sistema', 1),
-('Visualizar dashboards e alertas', 2),
-('Apenas visualização de relatórios', 3);
-
 
 INSERT INTO usuario (nome, cpf, email, senha, fk_empresa, fk_adm, fk_funcao) VALUES 
 ('José Valfogo', '11122233344', 'jose@valfogo.com', 'hash_senha_123', 1, NULL, 1),
@@ -154,37 +121,26 @@ INSERT INTO usuario (nome, cpf, email, senha, fk_empresa, fk_adm, fk_funcao) VAL
 ('Richard Oliveira', '99988877766', 'richard@valfogo.com', 'user_pass_456', 1, 1, 2);
 
 INSERT INTO servidor (nome, sistema_operacional, fk_empresa) VALUES 
-('SRV-Argos-01', 'Ubuntu 22.04 LTS', 1),
-('SRV-Argos-DB', 'Debian 11', 1),
-('SRV-DTIC-PROD', 'Windows Server 2022', 2);
-
-INSERT INTO servidor (nome, sistema_operacional, fk_empresa) VALUES 
-('luiz', 'Ubuntu 22.04 LTS', 2);
-/*
-INSERT INTO tipo_componente (id_tipo,fk_servidor, fk_formato, fk_componente, nome_componente, valor_max_critico, valor_min_critico,valor_max_atencao,valor_min_atencao) VALUES 
-(1, 1,1,1, 'cpu_percent', 12.00, 0.50,0.25,0.20),  
-(2, 2,2,1, 'ram_percent', 28.00, 22.00,12.00,10.00),    
-(3, 3,1,2, 'disk_percent', 90.00, 80.00,79.00,60.00), 
-(4, 1,2,2, 'total_processos', 87.00, 62.00,61.00,50.00);  
-*/
+('NB1-SRV-Argos-01', 'Ubuntu 22.04 LTS', 1),
+('NB1-SRV-Argos-DB', 'Debian 11', 1),
+('NB1-SRV-DTIC-PROD', 'Windows Server 2022', 2),
+('NB1-luiz', 'Ubuntu 22.04 LTS', 2);
 
 INSERT INTO tipo_componente (id_tipo,fk_servidor, fk_formato, fk_componente, nome_componente, valor_max_critico, valor_min_critico, capacidade) VALUES 
 (1, 1,1,1, 'cpu_percent', 12.00, 0.50,'3.7 Ghz'),  
 (2, 2,2,1, 'ram_percent', 28.00, 22.00, '16 GB'),    
 (3, 3,1,2, 'disk_percent', 90.00, 80.00, '1 TB'), 
-(4, 1,2,2, 'total_processos', 87.00, 62.00);  
+(4, 1,2,2, 'total_processos', 87.00, 62.00, null); 
 
 
 INSERT INTO tipo_componente 
-(fk_componente, fk_servidor, fk_formato, nome_componente, valor_max_critico, valor_min_critico, valor_max_atencao, valor_min_atencao) VALUES
-(1, 4, 1, 'cpu_percent', 90.00, 0.00, 75.00, 0.00),
-(1, 4, 2, 'cpu_freq_current', 4.00, 0.80, 3.50, 1.00),
-(2, 4, 1, 'ram_percent', 95.00, 0.00, 85.00, 0.00),
-(2, 4, 3, 'ram_used_gb', 14.00, 0.00, 12.00, 0.00),
-(3, 4, 1, 'disk_percent', 90.00, 0.00, 80.00, 0.00),
-(4, 4, 4, 'latencia_resposta_ms', 500.00, 0.00, 200.00, 0.00);
-
-desc tipo_componente;
+(fk_componente, fk_servidor, fk_formato, nome_componente, valor_max_critico, valor_min_critico) VALUES
+(1, 4, 1, 'cpu_percent', 90.00, 0.00),
+(1, 4, 2, 'cpu_freq_current', 4.00, 0.80),
+(2, 4, 1, 'ram_percent', 95.00, 0.00),
+(2, 4, 3, 'ram_used_gb', 14.00, 0.00),
+(3, 4, 1, 'disk_percent', 90.00, 0.00),
+(4, 4, 4, 'latencia_resposta_ms', 500.00, 0.00);
 
 INSERT INTO alerta (fk_tipo, fk_componente, descricao, valor_capturado) VALUES 
 (1, 1, 'Uso de CPU acima do limite operacional durante o pregão', 12.50),
@@ -195,14 +151,6 @@ INSERT INTO alerta (fk_tipo, fk_componente, descricao, valor_capturado) VALUES
 (1, 1, 'Uso de CPU acima do limite de atenção no servidor luiz', 78.50),
 (3, 2, 'Uso de RAM atingiu nível crítico no servidor luiz', 96.20),
 (2, 1, 'Pico de processamento detectado', 85.00);
-
- SELECT 
-	tipo.nome_componente, 
-    formato.unidade_medida 
-            FROM tipo_componente tipo
-            JOIN servidor ON tipo.fk_servidor = servidor.id_servidor
-            JOIN formato ON tipo.fk_formato = formato.id_formato
-            WHERE servidor.nome = 'luiz';
 
 CREATE VIEW tratamento AS
 SELECT 
@@ -232,6 +180,14 @@ select*from permissao;
 
 # ============================================================
 # SELECTS
+
+ SELECT 
+	tipo.nome_componente, 
+    formato.unidade_medida 
+            FROM tipo_componente tipo
+            JOIN servidor ON tipo.fk_servidor = servidor.id_servidor
+            JOIN formato ON tipo.fk_formato = formato.id_formato
+            WHERE servidor.nome = 'luiz';
 
 select*from servidor Where fk_empresa = 2;
 # drop table permissao;
